f5b6ed279c2bf38866ad5bd716b997aaaf7df181
Taras <tarasv290@gmail.com>
2020-03-17 17:48:06 +0200

    Add new functionality

---
+++ resources/views/settings/billing.blade.php
@@ -0,0 +1,106 @@
+@extends('app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+<div class="bg-gray-200 min-h-screen pb-24">
+    @include('partials.dashboard-header')
+
+    <div class="container mx-auto max-w-3xl mt-8">
+
+        <h1 class="text-2xl font-bold text-gray-700 px-6 md:px-0">Billing Settings</h1>
+        @include('settings.nav')
+        <form action="{{ route('billing.save') }}" id="billing-form" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="w-full bg-white rounded-lg mx-auto mt-8 flex overflow-hidden rounded-b-none">
+                <div class="w-1/3 bg-gray-100 p-8 hidden md:inline-block">
+                    <h2 class="font-medium text-md text-gray-700 mb-4 tracking-wide">Billing Settings</h2>
+                    <p class="text-xs text-gray-500">Update your billing info.</p>
+                </div>
+                <div class="md:w-2/3 w-full">
+                    @if(auth()->user()->subscribed('main'))
+                        <div class="py-8 px-16">
+                            <div>Thanks for being a subscriber</div>
+                            <div class="text-xs text-blue-600">Your default payment method ends in {{ auth()->user()->card_last_four }}</div>
+                            <div class="text-xs text-gray-500">To update your default payment method, add a new card below:</div>
+                        </div>
+                        <hr class="border-gray-300">
+                    @endif
+                    <div class="py-8 px-16">
+                        <label for="card-holder-name" class="text-sm text-gray-600">Name on Card</label>
+                        <input class="mt-2 border-2 border-gray-200 px-3 py-2 block w-full rounded-lg text-base text-gray-900 focus:outline-none focus:border-indigo-500" id="card-holder-name" type="text">
+                    </div>
+                    <hr class="border-gray-300">
+                    <div class="py-8 px-16">
+                        <label for="cc" class="text-sm text-gray-600">Credit Card</label>
+                        <div id="card-element" class="mt-2 border-2 border-gray-200 px-3 py-2 block w-full rounded-lg text-base text-gray-900 focus:outline-none focus:border-indigo-500"></div>
+                        <div id="card-errors" class="text-red-400 text-bold mt-2 text-sm font-medium"></div>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="p-16 py-8 bg-gray-300 clearfix rounded-b-lg border-t border-gray-200">
+                <p class="float-left text-xs text-gray-500 tracking-tight mt-2">Click on Save to update your Billing Info</p>
+                <button id="card-button" data-secret="{{ auth()->user()->createSetupIntent()->client_secret }}" class="bg-indigo-500 text-white text-sm font-medium px-6 py-2 rounded float-right uppercase cursor-pointer">
+                    Update Payment Method
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+
+@endsection
+
+@section('javascript')
+    <script src="https://js.stripe.com/v3/"></script>
+
+    <script>
+        const stripe = Stripe('{{ env("STRIPE_KEY") }}');
+
+        const elements = stripe.elements();
+        const cardElement = elements.create('card');
+
+        cardElement.mount('#card-element');
+
+        const cardHolderName = document.getElementById('card-holder-name');
+        const cardButton = document.getElementById('card-button');
+        const clientSecret = cardButton.dataset.secret;
+        const cardError = document.getElementById('card-errors');
+
+        cardElement.addEventListener('change', function(event) {
+            if (event.error) {
+                cardError.textContent = event.error.message;
+            } else {
+                cardError.textContent = '';
+            }
+        });
+
+        var form = document.getElementById('billing-form');
+        form.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            const { setupIntent, error } = await stripe.handleCardSetup(
+                clientSecret, cardElement, {
+                    payment_method_data: {
+                        billing_details: { name: cardHolderName.value }
+                    }
+                }
+            );
+            if (error) {
+                // Display "error.message" to the user...
+                cardError.textContent = error.message;
+            } else {
+                // The card has been verified successfully...
+                var hiddenInput = document.createElement('input');
+                hiddenInput.setAttribute('type', 'hidden');
+                hiddenInput.setAttribute('name', 'payment_method');
+                hiddenInput.setAttribute('value', setupIntent.payment_method);
+                form.appendChild(hiddenInput);
+                // Submit the form
+                form.submit();
+            }
+        });
+
+    </script>
+@endsection
---
+++ resources/views/settings/nav.blade.php
@@ -0,0 +1,5 @@
+<ul class="flex border-b border-gray-300 text-sm font-medium text-gray-600 mt-3 px-6 md:px-0">
+    <li class="mr-8 @if(Request::is('settings/profile')){{ 'text-indigo-600 border-b-2 border-indigo-500' }}@else{{ 'hover:text-gray-900' }}@endif"><a href="{{ route('profile') }}" class="py-4 inline-block">Profile</a></li>
+    <li class="mr-8 @if(Request::is('settings/security')){{ 'text-indigo-600 border-b-2 border-indigo-500' }}@else{{ 'hover:text-gray-900' }}@endif"><a href="{{ route('security') }}" class="py-4 inline-block">Security</a></li>
+    <li class="mr-8 @if(Request::is('settings/billing')){{ 'text-indigo-600 border-b-2 border-indigo-500' }}@else{{ 'hover:text-gray-900' }}@endif"><a href="{{ route('billing') }}" class="py-4 inline-block">Billing</a></li>
+</ul>
